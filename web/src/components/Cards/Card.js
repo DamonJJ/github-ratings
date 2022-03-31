9f81cc260972ed7ae0c5c8537074d2593c8fc9c9
DamonJJ <damon.jacoby@evereve.com>
2022-03-31 18:24:23 -0500

    Break cards data out into individual cards

---
+++ web/src/components/Cards/Card.js
@@ -0,0 +1,22 @@
+import './CardsStyles.scss'
+
+const Card = ({ data, mostStars, leastStars }) => {
+  return (
+    <div className="card" key={data.id}>
+      <div className="card-container" key={data.id}>
+        {mostStars && <h1>*Highest Stars*</h1>}
+        {leastStars && <h1>*Least Stars*</h1>}
+        {Object.entries(data).map(([key, value]) => {
+          return (
+            <>
+              <h2 className="card-label">{key}:</h2>
+              <p className="card-description">{value}</p>
+            </>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Card
