7469f7642448a2f6749d11a7c4cdbe187e7b9838
DamonJJ <damon.jacoby@evereve.com>
2022-03-31 14:55:16 -0500

    Add scss for Home Page

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import './HomePageStyles.scss'
 
 const HomePage = () => {
   const [userRepos, setUserRepos] = useState([])
