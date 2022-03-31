c720c5e8bf61db24547d051272736578149fa877
DamonJJ <damon.jacoby@evereve.com>
2022-03-31 14:54:51 -0500

    Sort repos by stars

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -2,6 +2,28 @@ import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
+  const [userRepos, setUserRepos] = useState([])
+
+  const onSubmit = (data) => {
+    fetch(`https://api.github.com/orgs/${data.user}/repos`)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json)
+        const data = json
+
+        const sortedReposByStars = data.sort((a, b) =>
+          a.stargazers_count < b.stargazers_count
+            ? 1
+            : a.stargazers_count === b.stargazers_count
+            ? a.size < b.size
+              ? 1
+              : -1
+            : -1
+        )
+
+        setUserRepos(sortedReposByStars)
+      })
+  }
   return (
     <>
       <MetaTags title="Home" description="Home page" />
