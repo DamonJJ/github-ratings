ddb37e1faf97ff5ac154eef59eea8e0cac3e7d68
DamonJJ <damon.jacoby@evereve.com>
2022-03-31 16:19:59 -0500

    Generate Search Bar component

---
+++ web/src/components/SearchBar/SearchBar.stories.js
@@ -0,0 +1,7 @@
+import SearchBar from './SearchBarStyles'
+
+export const generated = () => {
+  return <SearchBar />
+}
+
+export default { title: 'Components/SearchBar' }
---
+++ web/src/components/SearchBar/SearchBarStyles.js
@@ -0,0 +1,12 @@
+import './SearchBarStyle.scss'
+
+const SearchBar = () => {
+  return (
+    <div>
+      <h2>{'SearchBar'}</h2>
+      <p>{'Find me in ./web/src/components/SearchBar/SearchBar.js'}</p>
+    </div>
+  )
+}
+
+export default SearchBar
